13f2164b6ac613f2c59fd3153c76937286d7121a
MY <715711877@qq.com>
2023-05-11 17:43:44 +0800

    功能完善

--- src/dto/ACInfo.ts
+++ src/dto/ACInfo.ts
@@ -3,10 +3,10 @@ import BasicComponent from "./BasicComponent";
 export default interface ACInfo extends BasicComponent {
   state: 'on' | 'off' | 'unavailable'
   temperature: number
-  fanMode: string
+  fanMode: 'auto' | 'low' | 'medium' | 'high'
   fanModes: string[]
   hvacModes: string[]
-  hvacAction: string
+  hvacAction: 'off' | 'cooling'
   swingModes: string
   swingMode: string
   currentPower: number
--- src/view/component/my-ac/index.tsx
+++ src/view/component/my-ac/index.tsx
@@ -1,4 +1,4 @@
-import { Switch, Card, Statistic, Row, Col, Button, Tag, Progress, Space, Segmented   } from 'antd';
+import { Switch, Card, Statistic, Row, Col, Button, Tag, Progress, Space, Segmented } from 'antd';
 import ACInfo from '../../../dto/ACInfo';
 import { useState } from 'react';
 import {
@@ -7,49 +7,69 @@ import {
 } from '@ant-design/icons';
 import HomeAssistantService from '../../../service/HomeAssistantService'
 
+const fanModeMap = {
+  'auto': '自动',
+  'low': '低速',
+  'medium': '中速',
+  'high': '高速'
+}
 
-export default function MyAC(props: {acInfo: ACInfo}) {
+const hvacActionMap = {
+  'off': '关闭',
+  'cooling': '制冷',
+  'auto': '自动'
+}
+
+export default function MyAC(props: { acInfo: ACInfo }) {
   const [acInfo, setAcInfo] = useState(props.acInfo);
   const onSwitchChanged = async (checked: boolean) => {
-    let cmd = 'on'
+    let cmd = 'cool'
     if (!checked) {
       cmd = 'off'
     }
-    setAcInfo(await HomeAssistantService.executeCommand('climate/turn_' + cmd, acInfo.id, 'ac', {}) as ACInfo)
+    setAcInfo(await HomeAssistantService.executeCommand('climate/set_hvac_mode', acInfo.id, 'ac', {
+      hvac_mode: cmd
+    }) as ACInfo)
+  }
+  const setTemperature = async (tmp: number) => {
+    await onSwitchChanged(true)
+    setAcInfo(await HomeAssistantService.executeCommand('climate/set_temperature', acInfo.id, 'ac', {
+      temperature: tmp
+    }) as ACInfo)
   }
   return <div className='component'>
     <Card title={
       <div>
         <strong>{acInfo.name}</strong>
-        <Tag color="#2db7f5" style={{marginLeft: '6px'}}>{acInfo.room}</Tag>
+        <Tag color="#2db7f5" style={{ marginLeft: '6px' }}>{acInfo.room}</Tag>
       </div>
-    } extra={<Switch disabled={acInfo.state === 'unavailable'} checked={acInfo.state === 'on'} onChange={onSwitchChanged}/>}>
-      <Row style={{textAlign: 'center'}}>
-        <Col span={8}>
-          <div>
-            <Button><MinusOutlined /></Button><Button style={{marginLeft: '10px'}}><PlusOutlined /></Button>
-          </div>
-          <div style={{marginTop: '10px'}}>
-            <Button size='small' type="primary">24 ℃</Button>
-            <Button size='small' type="primary" style={{marginLeft: '5px  '}}>26 ℃</Button>
-          </div>
-          <div style={{marginTop: '10px'}}>
-            <Switch checkedChildren='扫风' unCheckedChildren="未扫风"/>
-          </div>
+    } extra={<Switch  disabled={acInfo.state === 'unavailable'} checked={acInfo.state !== 'off' && acInfo.state !== 'unavailable'} onChange={onSwitchChanged} />}>
+      <Row style={{ textAlign: 'center' }}>
+        <Col span={12}>
+          <Progress strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }} type="dashboard" percent={15 / acInfo.temperature * 100} gapDegree={90} format={(p) => acInfo.temperature + ' ℃'} />
         </Col>
-        <Col span={16}>
-          <Progress strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }} type="dashboard" percent={15 / acInfo.temperature * 100} gapDegree={90} format={(p) => acInfo.temperature + ' ℃'}/>
+        <Col span={12}>
+          <div>
+            <Button><MinusOutlined /></Button><Button style={{ marginLeft: '10px' }}><PlusOutlined /></Button>
+          </div>
+          <div style={{ marginTop: '10px' }}>
+            <Button size='small' type="primary" onClick={() => setTemperature(24)}>24 ℃</Button>
+            <Button size='small' type="primary" onClick={() => setTemperature(26)} style={{ marginLeft: '5px  ' }}>26 ℃</Button>
+          </div>
+          <div style={{ marginTop: '10px' }}>
+            <Switch checkedChildren='扫风' unCheckedChildren="未扫风" checked={acInfo.swingMode != 'off'} />
+          </div>
         </Col>
         <Col md={14} xs={24}>
-          <div style={{marginTop: '10px'}}>
-            <Segmented options={['自动', '低速', '中速', '高速']} />
+          <div style={{ marginTop: '10px' }}>
+            <Segmented value={fanModeMap[acInfo.fanMode]} options={['自动', '低速', '中速', '高速']} />
           </div>
-          <div style={{marginTop: '10px'}}>
-            <Segmented options={['自动', '制冷', '制热', '除湿', '送风', '关闭']} />
+          <div style={{ marginTop: '10px' }}>
+            <Segmented value={hvacActionMap[acInfo.hvacAction]} options={['关闭', '制冷', '自动', '制热', '除湿', '送风']} />
           </div>
         </Col>
       </Row>
-      <Row style={{marginTop: '10px'}}>
+      <Row style={{ marginTop: '10px' }}>
         <Col span={8}>
           <Statistic title="本月用电" value={acInfo.monthConsumption || '-'} suffix="度" />
         </Col>
@@ -60,6 +80,6 @@ export default function MyAC(props: {acInfo: ACInfo}) {
           <Statistic title="当前功耗" value={acInfo.currentPower || '-'} suffix="w" />
         </Col>
       </Row>
-    </Card> 
+    </Card>
   </div>
 }
